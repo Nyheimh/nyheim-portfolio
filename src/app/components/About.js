f8a22f31c9184f7955388911e43866751c620c40
Nyheim Hunter <78383071+Nyheimh@users.noreply.github.com>
2023-09-02 08:08:23 -0700

    Added the About section

---
+++ src/app/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <h1>Software Engineer with 2+ years of experience working in startups.</h1> 
+    <br />
+    <h2>I specialize in: <br />
+    <br />
+    - Frontend Development (Javascript, React, Typescript, Unit Testing, Jest, Redux, Next.js, CSS3, HTML5)<br />
+    - Backend Development (Node, Rails, Go MongoDB, PostgreSQL, SQL, Python)<br />
+    - Mobile Development (React Native, Flutter)<br />
+    - Development Technologies (Postman, Git, CI/CD, VS Code, Agile Methodologies, Figma, Github, Jira, Confluence)<br />
+    {" "}<br />
+    Researching methods and learning forms of AI. Outside of this, I am a father, and daily learner always attempting to increase my knowledge and staying adaptable in the approaches of life. </h2>
+    </>
+  )
+}
+
+export default About
