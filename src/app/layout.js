df4da87695e04c4cda02f0b6e1b0b6aae6112795
Nyheim Hunter <78383071+Nyheimh@users.noreply.github.com>
2024-06-14 10:55:41 -0700

    Added a demo link and og image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,19 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import portfolioImage from "../img/nyheim-hunter-portfolio.png";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Nyheim Hunter | Tech Enthusiast | Continous Learner ',
-  description: 'Nyheim Hunter`s software engineer portfolio',
-}
+  title: "Nyheim Hunter | Tech Enthusiast | Continous Learner ",
+  description: "Nyheim Hunter`s software engineer portfolio",
+  ogImage: portfolioImage,
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
